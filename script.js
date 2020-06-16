76d96d79c3493a4f2c736749fb556d87306da57a
Kacper Stokwisz <kacperstokwisz@gmail.com>
2020-06-16 21:29:34 +0200

    Add wrong letters & play again functions

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 const wordEl = document.getElementById('word');
 const wrongLettersEl = document.getElementById('wrong-letters');
-const playAgainBtn = document.getElementById('play-again');
+const playAgainBtn = document.getElementById('play-button');
 const popup = document.getElementById('popup-container');
 const notification = document.getElementById('notification-container');
 const finalMessage = document.getElementById('final-message');
 
-const figureParts = document.querySelectorAll('figure-part');
+const figureParts = document.querySelectorAll('.figure-part');
 
 const words = ['application', 'programming', 'interface', 'wizard'];
 
@@ -27,7 +27,29 @@ function displayWord() {
 
 // Update the wrong letters
 function updateWrongLettersEl() {
-    console.log('Update wrong');
+    //Display wrong letters
+    wrongLettersEl.innerHTML = `
+    ${wrongLetters.length > 0 ? '<p>Wrong</p>' : ''}
+    ${wrongLetters.map(letter => `<span>${letter}</span>`)}
+    `;
+
+    // Display parts
+    figureParts.forEach((part, index) => {
+        const errors = wrongLetters.length;
+
+        if(index < errors) {
+            part.style.display = 'block';
+        } else {
+            part.style.display = 'none';
+        }
+    })
+
+    // Check if lost
+    if(wrongLetters.length === figureParts.length) {
+        finalMessage.innerText = 'Unfortunately you lost'
+        popup.style.display = 'flex';
+    }
+
 }
 
 function showNotification() {
@@ -58,6 +80,18 @@ window.addEventListener('keydown', e => {
             }
         }
     }
+})
+
+// Restart game and play again
+playAgainBtn.addEventListener('click', () => {
+    // Empty arrays
+    correctLetters.splice(0);
+    wrongLetters.splice(0);
+    selectedWord = words[Math.floor(Math.random() * words.length)];
+
+    displayWord();
+    updateWrongLettersEl();
+    popup.style.display = 'none';
 })
 
 displayWord()
